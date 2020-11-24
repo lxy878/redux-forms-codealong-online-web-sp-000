6b493b61e01847355d0d9ec164be11eae7dd8a3e
Xiaoyi Liu <lxy878@gmail.com>
2020-11-24 14:21:20 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = e =>{
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+    this.props.addTodo(this.state)  
+  }
   render() {
     return(
       <div>
